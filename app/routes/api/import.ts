bd6da3ebaaa1faf48ed7073600e3336654eefcb0
Ian Lenehan <ianlenehan@gmail.com>
2023-12-15 22:11:19 +0000

    test prompt

--- app/routes/api/import.ts
+++ app/routes/api/import.ts
@@ -1,12 +1,45 @@
 import type { ActionArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
+import axios from 'axios'
+
+const apiKey = process.env.CHAT_GPT_KEY
+
+const prompt =
+  'I am going to provide you with the body of an email.' +
+  ' This email is a newsletter which contains various web links and descriptions of the websites. ' +
+  'Please determine the name of the newsletter, and the sender of the newsletter if that information ' +
+  'is available. Please then also provide a list of all the links in the newsletter as well as the ' +
+  'descriptions of each link. Do not summarise the description, use the description from the email. ' +
+  "Please give me all the data in JSON format. Like this: { newsletter: { name: 'Newsletter name', " +
+  "sender: 'some sender', issueNumber: 121, webLink: 'www.somelink.com' }, links: [{ title: 'This link', " +
+  "url: 'www.someurl.com', description: 'lorem ipsum'  }] }. The email is as follows:"
 
 export const action = async ({ request }: ActionArgs) => {
   switch (request.method) {
     case 'POST': {
       const payload = await request.json()
-      console.log('🚀 ~ file: import.ts:7 ~ action ~ payload:', payload)
-      return json({ success: true, payload }, 200)
+      const emailBody = payload.body
+
+      const res = await axios.post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          model: 'gpt-3.5-turbo',
+          messages: [{ role: 'user', content: prompt }],
+          temperature: 0.5,
+        },
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${apiKey}`,
+          },
+        }
+      )
+
+      const { choices } = res.data
+      const suggestion = JSON.parse(choices[0].message.content)
+
+      return json({ success: true, suggestion }, 200)
     }
   }
 }
